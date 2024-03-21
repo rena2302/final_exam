c223dee1822f745f622214c80615ffd64baeb800
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-03-21 13:36:29 +0700

    login and register

--- app/src/main/java/com/example/final_exam/CreateActivity.java
+++ app/src/main/java/com/example/final_exam/CreateActivity.java
@@ -2,13 +2,69 @@ package com.example.final_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class CreateActivity extends AppCompatActivity {
-
+    EditText emailInput,passwordInput,cfPasswordInput,userNameInput;
+    String email,pass,cfPass,userName;
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        emailInput=findViewById(R.id.email);
+         passwordInput=findViewById(R.id.password);
+        cfPasswordInput=findViewById(R.id.rePassword);
+        btnSubmit=findViewById(R.id.loginButton);
+        userNameInput=findViewById(R.id.username);
+        btnSubmit.setOnClickListener(v -> {
+            email=emailInput.getText().toString();
+            pass=passwordInput.getText().toString();
+            cfPass=cfPasswordInput.getText().toString();
+            userName=userNameInput.getText().toString();
+            if(email.isEmpty()||pass.isEmpty()||cfPass.isEmpty()||userName.isEmpty()){
+                Toast.makeText(CreateActivity.this,"Please enter all the fields",Toast.LENGTH_LONG).show();
+            }
+            else{
+                if(email.length()>=8){
+                    if(validEmail(email)){
+                        if(userName.length()>=6){
+                            if(pass.length()>6||cfPass.length()>6){
+                                if(pass.equals(cfPass)){
+                                    // success case
+                                    Intent intent  = new Intent(this, MainActivity.class);
+                                    intent.putExtra("userName",userName);
+                                    intent.putExtra("password",pass);
+                                    startActivity(intent);
+                                }
+                            }
+                            else{
+                                Toast.makeText(CreateActivity.this,"Enter enter password > 6 length!",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(CreateActivity.this,"Enter enter user name >= 6 length!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(CreateActivity.this,"Enter valid e-mail!",Toast.LENGTH_LONG).show();
+                    }
+                }
+                else{
+                    Toast.makeText(CreateActivity.this,"Enter enter email >= 8 length ",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    private boolean validEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
     }
 }
--- app/src/main/java/com/example/final_exam/MainActivity.java
+++ app/src/main/java/com/example/final_exam/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.final_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText userNameView,userPasswordView;
+    Button btn;
+    String name,password;
+    String userInputName,userInputPassword;
+    TextView toRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userNameView = findViewById(R.id.username);
+        userPasswordView = findViewById(R.id.password);
+        btn=findViewById(R.id.loginButton);
+        toRegister=findViewById(R.id.createNewAccount);
+        toRegister.setOnClickListener(v->{
+            Intent intent = new Intent(this,CreateActivity.class);
+            startActivity(intent);
+        });
+        name= getIntent().getStringExtra("userName");
+        password= getIntent().getStringExtra("password");
+        Log.d("userName", name);
+        Log.d("password", password);
+        if(name.isEmpty()||password.isEmpty()){
+            Toast.makeText(MainActivity.this,"Please register first",Toast.LENGTH_LONG).show();
+        }
+        else{
+            eventSubmit();
+        }
+
+    }
+    private void eventSubmit(){
+        btn.setOnClickListener(v->{
+            userNameView.findViewById(R.id.username);
+            userPasswordView.findViewById(R.id.password);
+            userInputName=userNameView.getText().toString();
+            userInputPassword=userPasswordView.getText().toString();
+            if(userInputName.isEmpty()||userInputPassword.isEmpty()){
+                Toast.makeText(MainActivity.this,"Enter enter all filed",Toast.LENGTH_LONG).show();
+            }
+            else {
+                if(userInputName.equals(name)){
+                    if(userInputPassword.equals(password)){
+                        //success case
+                        Log.d("LOGIN ", "Login success");
+
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"userName or password invalid",Toast.LENGTH_LONG).show();
+                    }
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"userName or password invalid",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
